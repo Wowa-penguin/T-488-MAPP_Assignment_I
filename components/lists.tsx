bacb728be2fe57e19466abd58b3b7d94e7038aad
borki1234 <bjarkihreinn@live.com>
2025-11-25 13:32:01 +0000

    Basic UI for lists

--- components/lists.tsx
+++ components/lists.tsx
@@ -1,14 +1,59 @@
-import data from '@/data/data.json';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const Lists = () => {
-  const lists = data.lists;
 
+type ListColumnProps = {
+  name: string;
+};
+
+
+const ListColumn = ({ name }: ListColumnProps) => {
   return (
-    <View>
-      <Text>Lists</Text>
+    <View style={styles.column}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>{name.toUpperCase()}</Text>
+      </View>
+
+      <View style={styles.card} />
+      <View style={styles.card} />
+      <View style={styles.card} />
     </View>
   );
 };
 
-export default Lists;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 16,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 16,
+  },
+  listsRow: {
+    paddingBottom: 16,
+  },
+  column: {
+    backgroundColor: '#deefffff',
+    padding: 12,
+    marginRight: 12,
+  },
+  header: {
+    marginBottom: 12,
+  },
+  headerText: {
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  card: {
+    height: 60,
+    borderRadius: 8,
+    backgroundColor: '#ffffffff',
+    marginBottom: 8,
+  },
+});
+
+export default ListColumn;
