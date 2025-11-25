50ecdaeb81464f09a81aba21e312b9e278002c0e
Wowa_penguin <Heimir2710@gmail.com>
2025-11-25 11:28:01 +0000

    Update index.tsx

--- app/tasks/index.tsx
+++ app/tasks/index.tsx
@@ -1,10 +1,41 @@
 import Tasks from '@/components/tasks';
+import data from '@/data/data.json';
+import { useLocalSearchParams } from 'expo-router';
 import { View } from 'react-native';
 
 const Index = () => {
+  const { listId } = useLocalSearchParams();
+
+  const tasks = data.tasks;
+  const renderTasks: {
+    id: number;
+    name: string;
+    description: string;
+    isFinished: boolean;
+    listId: number;
+  }[] = [];
+
+  tasks.forEach((task) => {
+    if (Number(listId) === task.listId) renderTasks.push(task);
+  });
+
   return (
     <View>
-      <Tasks />
+      {renderTasks.map((task) => (
+        <View
+          key={
+            (task.id, task.name, task.description, task.isFinished, task.listId)
+          }
+        >
+          <Tasks
+            id={task.id}
+            name={task.name}
+            description={task.description}
+            isFinished={task.isFinished}
+            listId={task.listId}
+          />
+        </View>
+      ))}
     </View>
   );
 };
