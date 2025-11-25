12da1adb26ba50834b9c8518c15076b68d1cebf9
Wowa_penguin <Heimir2710@gmail.com>
2025-11-25 10:39:30 +0000

    Virkar svona 50% af tímanum

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,57 @@
-import { StyleSheet, View } from 'react-native';
+import data from '@/data/data.json';
+import { StyleSheet, Text, View } from 'react-native';
 
 import Boards from '@/components/boards';
 
 export default function Index() {
+  const boards = data.boards;
+
   return (
-    <View style={styles.test}>
-      <Boards />
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Toodler</Text>
+      </View>
+      <View style={styles.mainBoard}>
+        {boards.map((board) => (
+          <Boards
+            key={(board.name, board.description, board.thumbnailPhoto)}
+            name={board.name}
+            description={board.description}
+            img={board.thumbnailPhoto}
+          />
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  test: {
-    flex: 1,
-    justifyContent: 'center',
+  header: {
+    paddingTop: 40,
+    paddingBottom: 20,
+    backgroundColor: '#f9f9f9',
+    borderBottomWidth: 1,
+    borderBottomColor: '#e0e0e0',
     alignItems: 'center',
+  },
+
+  headerTitle: {
+    fontSize: 30,
+    fontWeight: '900',
+    color: '#444',
+  },
+
+  mainBoard: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    paddingHorizontal: 10,
+    paddingTop: 20,
+    backgroundColor: '#f2f2f2',
   },
 });
--- components/boards.tsx
+++ components/boards.tsx
@@ -1,70 +1,28 @@
-import data from '@/data/data.json';
 import { Image, StyleSheet, Text, View } from 'react-native';
 
-const Boards = () => {
-  const boards = data.boards;
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Toodler</Text>
-      </View>
+type BoardProps = {
+  name: string;
+  description: string;
+  img: string;
+};
 
-      <View style={styles.mainBoard}>
-        {boards.map((board) => (
-          <View key={(board.name, board.description)} style={styles.board}>
-            <Image
-              source={{
-                width: 170,
-                height: 150,
-                uri: board.thumbnailPhoto,
-              }}
-            />
-            <Text style={styles.name}>{board.name}</Text>
-            <Text style={styles.description}>{board.description}</Text>
-          </View>
-        ))}
-      </View>
+const Boards = ({ name, description, img }: BoardProps) => {
+  return (
+    <View style={styles.board}>
+      <Image
+        source={{
+          width: '100%',
+          height: 150,
+          uri: img,
+        }}
+      />
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.description}>{description}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f2f2f2'
-  },
-
-// ---Header---
-
-  header: {
-  paddingTop: 40,
-  paddingBottom: 20,
-  backgroundColor: "#f9f9f9",
-  borderBottomWidth: 1,
-  borderBottomColor: "#e0e0e0",
-  alignItems: "center",
-},
-
-headerTitle: {
-  fontSize: 30,
-  fontWeight: "900",
-  color: "#444",
-},
-
-
-// ---Board grid---
-
-  mainBoard: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    flexWrap: 'wrap',
-    paddingHorizontal: 10,
-    paddingTop: 20,
-  },
-
-// ---Single card---
-
   board: {
     width: '45%',
     padding: 10,
@@ -75,7 +33,7 @@ headerTitle: {
     // --- iOS
     shadowColor: '#000',
     shadowOpacity: 0.15,
-    shadowOffset: {width: 0, height: 4},
+    shadowOffset: { width: 0, height: 4 },
     shadowRadius: 6,
 
     // --- Android
@@ -83,8 +41,8 @@ headerTitle: {
   },
 
   image: {
-    width: '100%',
-    height: 150,
+    width: '80%',
+    aspectRatio: 4 / 3,
     borderRadius: 12,
     marginBottom: 10,
   },
