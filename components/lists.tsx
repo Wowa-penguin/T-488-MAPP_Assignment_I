a74a65be8327d36f577598412e6c2c3d9a30f26b
vitaostim <164045322+vitaostim@users.noreply.github.com>
2025-11-27 12:14:53 +0000

    delete list alert

--- components/lists.tsx
+++ components/lists.tsx
@@ -9,6 +9,7 @@ import {
   Text,
   TextInput,
   View,
+  Alert,
 } from 'react-native';
 
 type ListProps = {
@@ -47,6 +48,21 @@ const Lists = ({ id, name, color, tasks, onAddTask }: ListProps) => {
     setEditModalVisible(false);
     setTaskName('');
     setTaskDescription('');
+  };
+
+  const confirmDeleteList = () => {
+    Alert.alert(
+      'Delete List',
+      `Are you sure you want to delete the list "${name}" and all its tasks?`,
+      [
+        { text: 'Cancel', style: 'cancel' },
+        {
+          text: 'Delete',
+          style: 'destructive',
+          onPress: () => deleteList(id),
+        },
+      ]
+    );
   };
 
   return (
@@ -103,7 +119,7 @@ const Lists = ({ id, name, color, tasks, onAddTask }: ListProps) => {
           <Button
             title="Delete List"
             color="red"
-            onPress={() => deleteList(id)}
+            onPress={confirmDeleteList}
           />
         </View>
       </View>
